b972d094e06396b67a9a76b0fee6944533177a5a
oli4vr <oli4vr@gmail.com>
2024-03-12 13:38:04 +0100

    RPM build

--- build_installer.sh
+++ build_installer.sh
@@ -28,9 +28,28 @@ make clean
 mv restit.*.sh ..
 cd ..
 chmod a+rx restit.*.sh
+if test -x /usr/bin/rpmbuild
+then
+ rm -rf ~/rpmbuild 2>/dev/null
+ cp restit.*.sh gpfsrest.sh
+ chmod u+x gpfsrest.sh
+ cat gpfs.restit.spec.templ | sed -e 's/__RELEASE__/'$(date "+%Y %j" | awk '{printf("%02d%03d\n",$(1)-2024,$2);}')'/' >gpfs.restit.spec
+ echo rpmbuild --define \"_sourcedir $(pwd)\" -bb gpfs.restit.spec
+ rpmbuild --define "_sourcedir $(pwd)" -bb gpfs.restit.spec >/dev/null 2>&1
+ mv $(find ~/rpmbuild/RPMS -name '*.rpm'  | head -n1) .
+ rm -rf ~/rpmbuild 2>/dev/null
+ rm gpfsrest.sh
+ rm gpfs.restit.spec
+fi
+RPMF=$(ls *.rpm 2>/dev/null | head -n1)
 echo "###################"
-echo "Installer Package :" $(ls -ltr restit.*.sh | tail)
+echo "Installer Package :" $(ls -tr restit.*.sh | tail)
 echo "Distribute and install on GPFS nodes"
 echo "Install with : ./"$(ls -tr restit.*.sh | tail)
 echo "Uninstall    : ./"$(ls -tr restit.*.sh | tail)" -u"
+if [ "$RPMF" != "" ]
+then
+ echo "RPM package  : ${RPMF}"
+fi
 echo "Access via   : http://hostname:40480"
+
